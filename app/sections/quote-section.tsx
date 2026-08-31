e23ace8cea6db853dd12623b9b525a25bc9b6a32
AlokPy1484 <alokpandey1483@gmail.com>
2026-09-01 01:02:24 +0530

    Footer section socials linked

--- app/sections/quote-section.tsx
+++ app/sections/quote-section.tsx
@@ -1,3 +1,4 @@
+import { JSX } from "react/jsx-runtime"
 
 
 
@@ -18,6 +19,30 @@ export default function QuoteSection() {
         <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" viewBox="0 0 256 256"><path d="M218.123 218.127h-37.931v-59.403c0-14.165-.253-32.4-19.728-32.4-19.756 0-22.779 15.434-22.779 31.369v60.43h-37.93V95.967h36.413v16.694h.51a39.907 39.907 0 0 1 35.928-19.733c38.445 0 45.533 25.288 45.533 58.186l-.016 67.013ZM56.955 79.27c-12.157.002-22.014-9.852-22.016-22.009-.002-12.157 9.851-22.014 22.008-22.016 12.157-.003 22.014 9.851 22.016 22.008A22.013 22.013 0 0 1 56.955 79.27m18.966 138.858H37.95V95.967h37.97v122.16ZM237.033.018H18.89C8.58-.098.125 8.161-.001 18.471v219.053c.122 10.315 8.576 18.582 18.89 18.474h218.144c10.336.128 18.823-8.139 18.966-18.474V18.454c-.147-10.33-8.635-18.588-18.966-18.453" fill="oklch(55.6% 0 none)" /></svg>
     )
 
+    type SocialsType = {
+        href: string,
+        icon: () => JSX.Element,
+        label: string
+    }
+
+    const socials: SocialsType[] = [
+        {
+            href: "https://github.com/AlokPy1484",
+            icon: githubSVG,
+            label: "Github"
+        },
+        {
+            href: "https://x.com/AlokPandey37305",
+            icon: xSVG,
+            label: "X"
+        },
+        {
+            href: "https://www.linkedin.com/in/alokpy1484/",
+            icon: linkedinSVG,
+            label: "Linkedin"
+        }
+    ]
+
 
     return (
         <div className="hero-container flex flex-col justify-start items-start gap-8  w-full max-w-2xl px-8 md:px-0 mt-20 pt-16 border-t border-neutral-800/80 border-dashed">
@@ -37,15 +62,15 @@ export default function QuoteSection() {
                     <a>Contact</a> */}
                 </div>
                 <div className="flex justify-end items-center gap-4">
-                    <div className="size-5">
-                        {githubSVG()}
-                    </div>
-                    <div className="size-5">
-                        {xSVG()}
-                    </div>
-                    <div className="size-5">
-                        {linkedinSVG()}
-                    </div>
+
+                    {socials.map((social, idx) => (
+
+
+                        <a key={idx} href={social.href} className="size-5 cursor-pointer" target="_blank">
+                            {social.icon()}
+                        </a>
+                    ))}
+
                 </div>
             </div>
         </div>
